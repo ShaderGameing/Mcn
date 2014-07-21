aa2cdf28b982a9493b0e75992b177e61c73f9c17
Antonio Patterson <dbmack98@gmail.com>
2014-07-21 12:22:52 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+	$name = 'MelonCraftPE - 0.9 '; //Your server name
+	$server_ip = 'mcnserver.info'; //Your server IP
+	$server_port = '19132';
+	$description = 'Welcome to MelonCraft Network. The fastest growing MCPE Server.'; //A brief description of your server
+	$store = '#'; //Link to your server's store
+	$contact = 'meloncraftnetwork@gmail.com'; //E-mail to contact
+
+	// Created by IanSzot \\
+?>
